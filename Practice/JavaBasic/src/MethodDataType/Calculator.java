6fc7df9c771ff891097b52d09a4de42e464e17e0
luongtd24 <ducluongp72402@gmail.com>
2023-04-26 19:39:28 +0700

    update file

--- Practice/JavaBasic/src/MethodDataType/Calculator.java
+++ Practice/JavaBasic/src/MethodDataType/Calculator.java
@@ -2,6 +2,15 @@ package MethodDataType;
 
 public class Calculator {
 
-    public int
+    public static int tong(int a, int b){
+        return a + b;
+    }
+    public static double tich (double c, double d){
+        return c * d;
+    }
+    public static void main(String[] args) {
+        System.out.println(Calculator.tong(20,30));
+        System.out.println(Calculator.tich(10, 5.5));
+    }
 
 }
