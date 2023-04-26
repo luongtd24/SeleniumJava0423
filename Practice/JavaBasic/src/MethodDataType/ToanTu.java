0eecfec76ea1740d0583df47175c7207f8d3a3ea
luongtd24 <ducluongp72402@gmail.com>
2023-04-26 16:01:30 +0700

    update file

---
+++ Practice/JavaBasic/src/MethodDataType/ToanTu.java
@@ -0,0 +1,18 @@
+package MethodDataType;
+
+public class ToanTu {
+    public static void main(String[] args) {
+        int a = 24;
+        int b = 2;
+
+        Boolean checkA = (a > 20) && (b < 20);
+        Boolean checkB = (a == 20 ) && (b > 20);
+        Boolean checkC = (a == 24) || (b > 20);
+        Boolean checkD = (a < 20) || (b == 20);
+
+        System.out.println(checkA);
+        System.out.println(checkB);
+        System.out.println(checkC);
+        System.out.println(checkD);
+    }
+}
